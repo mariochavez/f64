55fd4d746151370e8cf12334fae506109bb73f39
Alexis Chavez <alexischavez@Alexiss-MBP.lan>
2019-01-02 17:21:19 -0600

    finished

---
+++ assets/js/parallax.js
@@ -0,0 +1,16 @@
+var velocity = 0.5;
+
+function update(){ 
+  var pos = $(window).scrollTop(); 
+  $('.parallax').each(function() { 
+    var $element = $(this);
+    // subtract some from the height b/c of the padding
+    var height = $element.height()-18;
+    $(this).css('backgroundPosition', '50% ' + Math.round((height - pos) * velocity) +  'px'); 
+  }); 
+};
+
+$('div#second').scroll(function() {
+    didScroll = true;
+    console.log('hello there')
+});
